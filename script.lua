b56e8ab813cb4879d355a854b6c67bb5644ff1df
Leo <172087713+Rrrfsghdsrhgsf@users.noreply.github.com>
2026-06-25 19:59:18 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-print ("999")
+print ("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
